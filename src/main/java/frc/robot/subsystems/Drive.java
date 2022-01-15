db8b6844683989124b171c02de1a138f028414d0
szheng0312 <s.zheng25@bcp.org>
2022-01-14 17:11:15 -0800

    test

--- src/main/java/frc/robot/subsystems/Drive.java
+++ src/main/java/frc/robot/subsystems/Drive.java
@@ -5,31 +5,36 @@ import com.ctre.phoenix.motorcontrol.NeutralMode;
 import com.ctre.phoenix.motorcontrol.can.TalonFX;
 import com.ctre.phoenix.motorcontrol.FeedbackDevice;
 
-public class Drive {
-    private int right = 0;
-    private int right2 = 1;
-    private int left = 0;
-    private int left2 = 1;
+interface subsystems{
+    void init();
+    void setOpenLoop(double throttle, double turn);
+    void stop();
+}
+public class Drive implements subsystems{
+    private static final int kright = 0;
+    private static final int kright2 = 1;
+    private static final int kleft = 0;
+    private static final int kleft2 = 1;
 
-    private TalonFX rightMotor  = new TalonFX(right);
-    private TalonFX rightMotor2  = new TalonFX(right2);
-    private TalonFX leftMotor  = new TalonFX(left);
-    private TalonFX leftMotor2  = new TalonFX(left2);
+    private TalonFX rightMaster  = new TalonFX(kright);
+    private TalonFX rightMotor2  = new TalonFX(kright2);
+    private TalonFX leftMaster  = new TalonFX(kleft);
+    private TalonFX leftMotor2  = new TalonFX(kleft2);
 
     public void init(){
-        rightMotor2.set(ControlMode.Follower, right);
-        leftMotor2.set(ControlMode.Follower, left);
-        rightMotor.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Relative, 0, 1000);
-        leftMotor.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Relative, 0, 1000);
+        rightMotor2.set(ControlMode.Follower, kright);
+        leftMotor2.set(ControlMode.Follower, kleft);
+        rightMaster.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Relative, 0, 1000);
+        leftMaster.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Relative, 0, 1000);
     }
 
     public void setOpenLoop(double throttle, double turn){
-        rightMotor.set(ControlMode.PercentOutput, throttle + turn);
-        leftMotor.set(ControlMode.PercentOutput, throttle - turn);
+        rightMaster.set(ControlMode.PercentOutput, throttle + turn);
+        leftMaster.set(ControlMode.PercentOutput, throttle - turn);
     }
     public void stop(){
-        rightMotor.setNeutralMode(NeutralMode.Brake);
-        leftMotor.setNeutralMode(NeutralMode.Brake);
+        rightMaster.setNeutralMode(NeutralMode.Brake);
+        leftMaster.setNeutralMode(NeutralMode.Brake);
     }
 
 
